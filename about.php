444b5975077ab49818ac5c6834b323e070d978c5
arvi3674 <arvind.kumar@truckx.com>
2023-07-15 19:41:49 +0530

    v0.0.1, changed the file name of index

--- about.php
+++ about.php
@@ -19,7 +19,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active nav-text" aria-current="page" href="index.php">HOME</a>
+                        <a class="nav-link active nav-text" aria-current="page" href="index.html">HOME</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link nav-text" href="about.php">ABOUT</a>
--- index.php
+++ index.php
@@ -20,7 +20,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active nav-text" aria-current="page" href="index.php">HOME</a>
+                        <a class="nav-link active nav-text" aria-current="page" href="index.html">HOME</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link nav-text" href="#">ABOUT</a>
