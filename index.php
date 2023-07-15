22d42a1dfc7a07a5bd337b248a199a8c31212125
arvi3674 <arvind.kumar@truckx.com>
2023-07-15 19:35:58 +0530

    v0.0.1, added menu page

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link href="style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>CHEESY HUNGER</title>
+    <title>CHEESY HUNGER | INDIA</title>
 </head>
 <body>
 <div class="px-2 py-1 card progress-div">Website in Progress...</div> 
@@ -38,7 +38,7 @@
                         <a class="nav-link nav-text" href="#">FRANCHAISE MODEL</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link nav-text" href="#">MENU</a>
+                        <a class="nav-link nav-text" href="menu.php">MENU</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link nav-text" href="#contact">CONTACT</a>
@@ -120,6 +120,7 @@
                     <video src="assets/video/video13.mp4" alt="..." autoplay loop muted class="d-block w-100"></video>
                     </div>
                 </div>
+            </div>
         </section>
         <!-- Today's speciality end -->
 
--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@
     <link href="style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>CHEESY HUNGER | Menu</title>
+    <title>CHEESY HUNGER | MENU</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light">
@@ -22,7 +22,7 @@
                         <a class="nav-link active nav-text" aria-current="page" href="index.php">HOME</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link nav-text" href="about.php">ABOUT</a>
+                        <a class="nav-link nav-text" href="#">ABOUT</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle nav-text" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -34,7 +34,7 @@
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link nav-text" href="franchise.php">FRANCHAISE MODEL</a>
+                        <a class="nav-link nav-text" href="#">FRANCHAISE MODEL</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link nav-text" href="menu.php">MENU</a>
@@ -47,10 +47,30 @@
         </div>
     </nav>
 
-    <main>
+    <div class="px-2 mb-2 franchise-container">
+        <div class="franchise-div card px-3 mt-4" style="float:left;">
+            <p class="pb-0 mb-0 franchise-enq">Franchise Enquiry</p>
+            <p class="text-center franchise-enq-no mb-0"><a href="tel:09955002277" class="nav-link p-0 franchise-enq-no"><i class="fa">&#xf3cd;</i> 9955002277</a></p>
+        </div>
+
+        <div class="p-2 card social-div" style="float:right;">
+            <div>
+                <a href="https://www.instagram.com/cheesyhunger_india/" target="_blank" class="social-icon"><i class="fab fa-instagram" style="padding-left:1px;"></i></a>
+            </div>
+            <div>
+                <a href="https://www.facebook.com/cheesyhungerindia" target="_blank" class="social-icon" style="color:0000ff;"><i class="fab fa-facebook"></i></a>
+            </div>
+            <div>
+                <a href="https://wa.me/9955002277" target="_blank" class="social-icon" style="color:#25D366;padding-left:1px;"><i class="fab fa-whatsapp"></i></a>
+            </div>
+        </div>
+        <div style="clear:both;"></div>
+    </div>
+
+    <main style="background-color:black;">
 
         <!-- Menu start -->
-        <section id="menu" class="mb-3">
+        <section id="menu" class="pb-3" style="margin-bottom:1px;">
             <div class="menu-container py-2">
                 <div class="py-1 d-flex justify-content-center">
                     <p class="para-above">Tasty Menu</p>
@@ -72,22 +92,23 @@
     </main>
 
     <!-- Footer start -->
-    <div class="middle contact-div bg-light pt-4 pb-0" id="contact-1">
+    <div class="middle contact-div pt-5 pb-0 contact-div2" id="contact-1">
                 <h3 class="middle-head" style="font-weight:bold;color:#ceba28;">Get in Touch</h3>
                 <a href="https://twitter.com/cheesyhunger" target="_blank" class="social-icon"><i class="fab fa-twitter">&nbsp;</i></a>
                     <a href="https://www.instagram.com/cheesyhunger_india/" target="_blank" class="social-icon"><i class="fab fa-instagram">&nbsp;</i></a>
-                    <a href="https://www.facebook.com/cheesyhungerindia" target="_blank" style="color:blue;"><i class="fab fa-facebook">&nbsp;</i></a>
+                    <a href="https://www.facebook.com/cheesyhungerindia" target="_blank" style="color:0000ff;"><i class="fab fa-facebook">&nbsp;</i></a>
                     <a href="https://wa.me/9955002277" target="_blank" style="color:#25D366;"><i class="fab fa-whatsapp">&nbsp;</i></a>
+                    <a href="https://www.youtube.com/@CHEESYHUNGERINDIA" target="_blank" style="color:red;"><i class="fab fa-youtube">&nbsp;</i></a>
                     <a href="https://www.linkedin.com/in/cheesy-hunger-52492126a/?jobid=1234" target="_blank" style="color:#0072b1;"><i class="fab fa-linkedin"></i></a>    
     </div>
-    <div class="bg-light w-100" id="contact">
+    <div class="bg-light w-100 contact-div1" id="contact">
         <footer class="py-3 footer-div">
             <div class="container-div">
                 <div class="left contact-div">
                     <h3 style="font-weight:bold;color:rgb(238, 150, 43);">Opening Hours</h3>
                     <ul class="nav flex-column">
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa fa-calendar" aria-hidden="true" style="color:#cc0f90;"></i> Mon - Sun</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa" style="color:#ceba28;">&#xf017;</i> 10 AM - 10 PM</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa" style="color:#ceba28;">&#xf017;</i> 11 AM - 10 PM</a></li>
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa" style="color:red;">&#xf3c5;</i> Bihar, India</a></li>
                     </ul>
                 </div>
@@ -95,15 +116,15 @@
                 <h3 class="middle-head" style="font-weight:bold;color:#ceba28;">Get in Touch</h3>
                     <a href="https://twitter.com/cheesyhunger" target="_blank" class="social-icon"><i class="fab fa-twitter">&nbsp;</i></a>
                     <a href="https://www.instagram.com/cheesyhunger_india/" target="_blank" class="social-icon"><i class="fab fa-instagram">&nbsp;</i></a>
-                    <a href="https://www.facebook.com/cheesyhungerindia" target="_blank" style="color:blue;"><i class="fab fa-facebook">&nbsp;</i></a>
+                    <a href="https://www.facebook.com/cheesyhungerindia" target="_blank" style="color:0000ff;"><i class="fab fa-facebook">&nbsp;</i></a>
                     <a href="https://wa.me/9955002277" target="_blank" style="color:#25D366;"><i class="fab fa-whatsapp">&nbsp;</i></a>
-                    <!-- <a href="https://www.youtube.com/your-channel" target="_blank" style="color:red;"><i class="fab fa-youtube">&nbsp;</i></a> -->
+                    <a href="https://www.youtube.com/@CHEESYHUNGERINDIA" target="_blank" style="color:red;"><i class="fab fa-youtube">&nbsp;</i></a>
                     <a href="https://www.linkedin.com/in/cheesy-hunger-52492126a/?jobid=1234" target="_blank" style="color:#0072b1;"><i class="fab fa-linkedin"></i></a>    
                 </div>
                 <div class="right contact-div">
                     <h3 class="right-head" style="font-weight:bold;color:rgb(238, 150, 43);">Contact Us</h3>
                     <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">info@cheesyhunger.com <i class="fa" style="color:rgb(196, 187, 6);">&#xf0e0;</i></a></li>
+                    <li class="nav-item mb-2"><a href="mailto:info@cheesyhunger.com" class="nav-link p-0 text-muted">info@cheesyhunger.com <i class="fa" style="color:rgb(196, 187, 6);">&#xf0e0;</i></a></li>
                     <li class="nav-item mb-2"><a href="tel:09955002277" class="nav-link p-0 text-muted">Call us for franchise <i class="fa" style="color:rgb(158, 78, 232);">&#xf095;</i></a></li>
                     <li class="nav-item mb-2"><a href="tel:09955002277" class="nav-link p-0 text-muted">+91-9955002277 <i class="fa" style="color:rgb(240, 69, 169);">&#xf3cd;</i></a></li>
                     </ul>
@@ -111,13 +132,14 @@
             </div>
         </footer>
     </div>  
-    <div class="d-flex flex-column flex-sm-row justify-content-between border-top bg-danger py-3 px-4">
-            <p class="mx-auto">Copyright &copy; 2023 Cheesy Hunger, Inc. All rights reserved.</p>
+    <div class="d-flex flex-column flex-sm-row justify-content-between border-top py-2 px-4 copyright-foot">
+            <p class="mx-auto copyright-para">Copyright &copy; 2023 Cheesy Hunger, Inc. All rights reserved.</p>
     </div> 
     <!-- Footer end-->
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script> -->
 </body>
 </html>
